aa31f55ecaa3f13140ea0765542c39edac72e474
amethystdesign <amethystdesignca@hotmail.com>
2025-04-19 14:49:49 -0400

    test4

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
-// import Footer from "@/components/Footer";
+// import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
 // import Clients from "@/components/Clients";
 // import Approach from "@/components/Approach";
 // import Experience from "@/components/Experience";
@@ -20,14 +20,14 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid />
+        {/* <Grid /> */}
         {/* <RecentProjects /> */}
         {/* <WorkExperience /> */}
         {/* <Clients /> */}
         {/* <Experience /> */}
         {/* <Certification /> */}
         {/* <Approach /> */}
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </main>
   );
